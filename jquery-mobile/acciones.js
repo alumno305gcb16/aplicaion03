e4d988936538a8dee7b6616a90355ad07262a013
alumno305gcb16 <nancytg@gmail.com>
2014-10-29 13:59:53 -0600

    primeraa
    si

---
+++ jquery-mobile/acciones.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+$(document).ready(function(e){
+	document.addEventListener("deviceready",function(){
+		
+		$('#beep').tap(function(){
+			navigator.notificacion.beep(1);
+		});//tap beep
+		
+		$('#vibrar').tap(function(){
+			navigator.notificacion.vibrate(1000);
+		});//tap vibrar
+		
+		
+	},false); //deviceready
+});//ready
